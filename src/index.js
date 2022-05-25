1f97518e7d35e5c2cbecb5789dcbc8d7454c235e
Łukasz Pękala <lukasz-pekala@users.noreply.github.com>
2022-05-25 16:21:18 +0200

    code cleanup

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useReducer } from 'react';
+import React, { useReducer } from 'react';
 import ReactDOM from 'react-dom';
 
 import { BrowserRouter as Router } from 'react-router-dom';
